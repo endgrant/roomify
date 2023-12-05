84b7df1f0993bbbb3abebd4cebbccf4fa2e9ce02
NathanaelRobbins <112914338+Nathanael-Robbins@users.noreply.github.com>
2023-12-05 16:39:31 -0500

    Volume Value
    added a static variable for volume to the audio script

--- Audio/AudioModifier.cs
+++ Audio/AudioModifier.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class AudioModifier : AudioStreamPlayer {
 	private HSlider slider;
+	private static float volume = 0;
 
 	public override void _Ready() {
 		base._Ready();
@@ -12,6 +13,7 @@ public partial class AudioModifier : AudioStreamPlayer {
 	}
 
 	public void SliderChanged(float value) {
+		volume = value;
 		SetDeferred("volume_db", value);
 	}
 }
