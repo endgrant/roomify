681dd48ea788eb3f4f9809d902e2d474e2a6ab02
Grant <53729381+endgrant@users.noreply.github.com>
2023-12-08 13:45:38 -0500

    Fixed Level Saving/Loading

--- Menus/LevelViewer/LevelEditor/LevelEditor.cs
+++ Menus/LevelViewer/LevelEditor/LevelEditor.cs
@@ -136,11 +136,10 @@ public partial class LevelEditor : LevelViewer {
                                         break;
                                 }
                         }
-                } else {
-                        level.currentRoom.SetRoomData((Godot.Collections.Dictionary<string, Variant>)Json.ParseString(level.currentRoom.Save()));
-                        
+                } else if (level.currentRoom.GetRoomData() == null) {
+                        level.currentRoom.SetRoomData((Godot.Collections.Dictionary<string, Variant>)Json.ParseString(level.currentRoom.Save()));    
                 }
-                
+
                 level.Save(level.currentRoom.GetRoomData());
         }
 
