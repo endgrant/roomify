abc5fee9d8d5d9d748699aebf199d13eac594d0d
Grant <53729381+endgrant@users.noreply.github.com>
2023-12-09 13:19:12 -0500

    Changed ToggleBlock inheritance

--- Blocks/Basic/Toggleable/ToggleBlock.cs
+++ Blocks/Basic/Toggleable/ToggleBlock.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public abstract partial class ToggleBlock : BasicBlock {
+public abstract partial class ToggleBlock : AbstractBasic {
 	// The toggle block scene inherits the basic block scene, meaning that
 	// anything in the basic block scene will also automatically be applied
 	// to the toggle block, such as sprite or hitbox
@@ -10,10 +10,15 @@ public abstract partial class ToggleBlock : BasicBlock {
 	public override void _Ready() {
 		sprite = GetNode<Sprite2D>("Sprite2D");
 		hitbox = GetNode<CollisionShape2D>("CollisionShape2D");
-		ToggleHandler.instance.Connect(ToggleHandler.SignalName.Toggle, new Callable(this, "ChangeState"));
+
+                // ToggleBlock.cs
+		ToggleHandler.instance.Connect(
+                        ToggleHandler.SignalName.Toggle, new Callable(this, "ChangeState"));
 		ChangeState(ToggleHandler.instance.GetIsRed());
 	}
 
 
+        // ToggleBlock.cs
+        // Observes when the toggle switch signals activation
         protected abstract void ChangeState(bool isRed);
 }
